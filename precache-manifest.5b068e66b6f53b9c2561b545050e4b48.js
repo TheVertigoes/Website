253f6b79e13d0f6a48892c2ed9e71aa9dc7891c1
Aksel Glyholt <akselglyholt1@gmail.com>
2023-03-13 21:44:30 +0100

    Updates

---
+++ precache-manifest.5b068e66b6f53b9c2561b545050e4b48.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "339d46f369cd4a19ee28",
+    "url": "/static/css/main.8b55b9f6.chunk.css"
+  },
+  {
+    "revision": "339d46f369cd4a19ee28",
+    "url": "/static/js/main.339d46f3.chunk.js"
+  },
+  {
+    "revision": "e459cc6ec89340cad24c",
+    "url": "/static/js/1.e459cc6e.chunk.js"
+  },
+  {
+    "revision": "c20ac3ace8cd92531804",
+    "url": "/static/css/2.f776e7f5.chunk.css"
+  },
+  {
+    "revision": "c20ac3ace8cd92531804",
+    "url": "/static/js/2.c20ac3ac.chunk.js"
+  },
+  {
+    "revision": "9533ad4d11ae861fee58",
+    "url": "/static/js/runtime~main.9533ad4d.js"
+  },
+  {
+    "revision": "54556d42d26788c197796bbe51ff7e30",
+    "url": "/static/media/Logo.54556d42.png"
+  },
+  {
+    "revision": "e87f70568c0cd1ff7729205d37ced75a",
+    "url": "/static/media/Header_1.e87f7056.jpg"
+  },
+  {
+    "revision": "2d78d2cd06161f2426a5dad0eb0635c3",
+    "url": "/static/media/Header_2.2d78d2cd.jpg"
+  },
+  {
+    "revision": "17689947c4db47637e0669f94abf627a",
+    "url": "/static/media/Header_3.17689947.jpg"
+  },
+  {
+    "revision": "aa631491d4ab2edbd8eeffeec656b105",
+    "url": "/static/media/Header_4.aa631491.jpg"
+  },
+  {
+    "revision": "f42b97740749d3480b07732216430ae4",
+    "url": "/static/media/Header_5.f42b9774.jpg"
+  },
+  {
+    "revision": "27696dab9797b0f25e6212d7d605e33e",
+    "url": "/index.html"
+  }
+];
