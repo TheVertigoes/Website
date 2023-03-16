65daada8e2065da7a177c183c8f3552e28b46c41
Aksel Glyholt <akselglyholt1@gmail.com>
2023-03-16 23:29:35 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e70ed03174710ce7de685c372f705609.js"
+  "/precache-manifest.8f36beeab5c2dc02ef58ae5efab1963d.js"
 );
 
 workbox.clientsClaim();
